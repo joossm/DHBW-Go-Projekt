a9432e6486d5c822f20845bfd82374920a13e6ba
Matthias Jooß <Jooss.Matthias@gmail.com>
2021-10-19 15:53:00 +0200

    fick dich emil

--- src/main/xml_reader.go
+++ src/main/xml_reader.go
@@ -10,29 +10,29 @@ import (
 var path = "D:\\Benutzer\\DHBW-Doks\\Semester5\\Vorlesungen\\Go\\Abgabe\\GoProjekt\\assets\\locations.xml" // absolute Path cause the relative is not working right now
 var locations Locations
 
-type Locations struct{
-	XMLName xml.Name      `xml:"locations"`
-	Locations  []Location `xml:"location"`
+type Locations struct {
+	XMLName   xml.Name   `xml:"locations"`
+	Locations []Location `xml:"location"`
 }
-type Location struct{
+type Location struct {
 	XMLName xml.Name `xml:"location"`
-	Name string `xml:"name"`
+	Name    string   `xml:"name"`
 }
 
-func main(){
-	locations = read();
+func main() {
+	locations = read()
 }
-func (this Locations) GetLocations() []Location{
-	return this.Locations;
+func (this Locations) GetLocations() []Location {
+	return this.Locations
 }
-func (this Locations) getLength() int{
+func (this Locations) getLength() int {
 	return len(this.Locations)
 }
-func (Location) printName() string{
+func (Location) printName() string {
 	return "To Do"
 }
-func read() Locations{
-	xmlFile,_ := os.Open(path)
+func read() Locations {
+	xmlFile, _ := os.Open(path)
 	var locations Locations
 	fmt.Println("Successfully Opened " + path)
 	// defer the closing of our xmlFile so that we can parse it later on
@@ -40,14 +40,11 @@ func read() Locations{
 
 	byteValue, _ := ioutil.ReadAll(xmlFile)
 
-
-
 	xml.Unmarshal(byteValue, &locations)
-	for i := 0; i < len(locations.Locations); i++{
+	for i := 0; i < len(locations.Locations); i++ {
 		fmt.Println(locations.Locations[i].Name) //for now
 	}
 
 	return locations
 
 }
-
--- src/main/xml_reader_test.go
+++ src/main/xml_reader_test.go
@@ -3,13 +3,14 @@ package main
 import (
 	"testing"
 )
-func TestRead(t *testing.T){
+
+func TestRead(t *testing.T) {
 	a := read()
-	if a.getLength()<=0 {
+	if a.getLength() <= 0 {
 		t.Error("The locations List is empty")
-	}else{
-		for i := 0; i < len(a.Locations); i++{
-			if a.Locations[i].Name == ""{
+	} else {
+		for i := 0; i < len(a.Locations); i++ {
+			if a.Locations[i].Name == "" {
 				t.Error("Location name may not be empty")
 			}
 		}
