10e080e5a8a1a32603247dc6ce463ce22f5c29db
Torik23 <73279120+Torik23@users.noreply.github.com>
2021-10-27 13:04:37 +0200

    entered cmd

---
+++ cmd/cmd.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter the day you are looking for. (Day Format: DD.MM.YYYY)")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+		if strings.Compare("hi", text) == 0 {
+			fmt.Println("Hello, Yourself")
+			validDateFormat("23.13.2323")
+			break
+		}
+	}
+
+}
+
+func validDateFormat(date string) bool {
+	result := true
+	var compareOne []string
+	compareOne = strings.Split(date, ".")
+	for index, element := range compareOne {
+		if index == 0 || index == 1 {
+			if len(element) != 2 {
+				return false
+			}
+			for _, innerElement := range element {
+				if innerElement < 48 || innerElement > 57 {
+					return false
+				}
+			}
+		} else if index == 3 {
+			if len(element) != 4 {
+				return false
+			}
+			for _, innerElement := range element {
+				if innerElement < 48 || innerElement > 57 {
+					return false
+				}
+			}
+		} else {
+			return false
+		}
+	}
+
+	return result
+
+}
---
+++ cmd/cmd_test.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestValidDate(t *testing.T) {
+	validDateFormat("12.12.1221")
+	/*assert.Equal(t, validDateFormat("23.10.1991"), true)
+	assert.Equal(t, validDateFormat("2.10.1991"), false)
+	assert.Equal(t, validDateFormat("12.1.1991"), false)
+	assert.Equal(t, validDateFormat("12.10.19911"), false)
+	assert.Equal(t, validDateFormat("2.10.1991.12"), false)
+	assert.Equal(t, validDateFormat("1991.10.02"), false)
+	assert.Equal(t, validDateFormat("02.1991.10"), false)
+	assert.Equal(t, validDateFormat("aa.aaaa.%%"), false)
+	assert.Equal(t, validDateFormat("23.A2.2013"), false)*/
+}
