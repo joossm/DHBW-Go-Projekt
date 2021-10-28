1b4cebe9d3d54f89cb9977cd246d5cc30e3a08e8
Matthias Jooß <Jooss.Matthias@gmail.com>
2021-10-28 15:19:14 +0200

    Ich bin ein Gott, betet in richtung meines Hauses!
    Token Duration Flag

--- src/model/config/configuration_test.go
+++ src/model/config/configuration_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	_ "github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestInit(t *testing.T) {
@@ -15,25 +16,22 @@ func TestInit(t *testing.T) {
 	if port2 != "8444" {
 		t.Error("port 2 is ", port2, "but it should be :8444")
 	}
-	if tokenDuration != "60" {
+	if tokenDuration != time.Minute*5 {
 		t.Error("token duration is ", tokenDuration, "but it should be 60")
 	}
 	if xmlPath == "" {
 		t.Error("path is empty")
 	}
-	assert.Equal(t, flag.Parsed(),true)
+	assert.Equal(t, flag.Parsed(), true)
 
-	assert.False(t, flag.Lookup("port1")==nil)
-	assert.False(t, flag.Lookup("port2")==nil)
-	assert.False(t, flag.Lookup("tokenDuration")==nil)
-	assert.False(t, flag.Lookup("xmlPath")==nil)
+	assert.False(t, flag.Lookup("port1") == nil)
+	assert.False(t, flag.Lookup("port2") == nil)
+	assert.False(t, flag.Lookup("tokenDuration") == nil)
+	assert.False(t, flag.Lookup("xmlPath") == nil)
 
-	assert.Equal(t, port1,flag.Lookup("port1").Value.String(),true)
-	assert.Equal(t, port2,flag.Lookup("port2").Value.String(),true)
-	assert.Equal(t, tokenDuration,flag.Lookup("tokenDuration").Value.String(),true)
-	assert.Equal(t, xmlPath,flag.Lookup("xmlPath").Value.String(),true)
-
-
-
+	assert.Equal(t, port1, flag.Lookup("port1").Value.String(), true)
+	assert.Equal(t, port2, flag.Lookup("port2").Value.String(), true)
+	assert.Equal(t, tokenDuration, flag.Lookup("tokenDuration").Value.String(), true)
+	assert.Equal(t, xmlPath, flag.Lookup("xmlPath").Value.String(), true)
 
 }
