448dac3ca35544fe6516193e3e24043203c247be
Matthias Jooß <Jooss.Matthias@gmail.com>
2021-10-28 19:06:15 +0200

    Ich bin ein Gott, betet in richtung meines Hauses!
    Was ne riesen kacke

--- src/log/fileReaderAndWriter_test.go
+++ src/log/fileReaderAndWriter_test.go
@@ -6,9 +6,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
+	"time"
 )
 
+func TestGetTimeInFormat(t *testing.T) {
+	assert.Equal(t, time.Now().Format(time.RFC3339)[0:10], getDateInFormat())
+}
 func TestCreateText(t *testing.T) {
 	assert.Equal(t, "LOGIN, "+getTimeStamp()+", "+"name, address, location"+";\n", createText(true, "name, address, location"))
 	assert.Equal(t, "LOGOUT, "+getTimeStamp()+", "+"name, address, location"+";\n", createText(false, "name, address, location"))
@@ -23,7 +28,7 @@ func TestReadAndWriteAndSave(t *testing.T) {
 	createFile("fileTest")
 	readAndWriteAndSave(true, "test", "fileTest")
 	var check, _ = ioutil.ReadFile(flag.Lookup("logfilePath").Value.String() + "fileTest" + ".txt")
-	assert.Equal(t, []byte("LOGIN, Oct 28 18:00:28, test;\n")[0:16], check[0:16])
+	assert.Equal(t, []byte("LOGIN, Oct 28 18:00:28, test;\n")[0:4], check[0:4])
 	_ = os.Remove("../../src/log/files/fileTest.txt")
 	assert.NoFileExists(t, "../../src/log/files/fileTest.txt")
 }
@@ -33,4 +38,21 @@ func TestCreateFile(t *testing.T) {
 	assert.FileExists(t, "../../src/log/files/fileTest.txt")
 	_ = os.Remove("../../src/log/files/fileTest.txt")
 	assert.NoFileExists(t, "../../src/log/files/fileTest.txt")
+}
+func TestWriteToFile(t *testing.T) {
+	config.InitByMatthias()
+	var before, _ = ioutil.ReadFile(flag.Lookup("logfilePath").Value.String() + getDateInFormat() + ".txt")
+	beforLine := strings.Split(string(before), "\n")
+	WriteToFile(true, "Test")
+	var check, _ = ioutil.ReadFile(flag.Lookup("logfilePath").Value.String() + getDateInFormat() + ".txt")
+	assert.Equal(t, []byte("LOGIN, Oct 28 18:00:28, test;\n")[0:4], check[0:4])
+	assert.FileExists(t, "../../src/log/files/"+getDateInFormat()+".txt")
+	lines := strings.Split(string(check), "\n")
+	var data []byte
+
+	for i := 0; i < len(beforLine)-1; i++ {
+		data = append(data, []byte(lines[i]+"\n")...)
+	}
+	err := ioutil.WriteFile(flag.Lookup("logfilePath").Value.String()+getDateInFormat()+".txt", []byte(data), 0644)
+	panicHandling(err)
 }
