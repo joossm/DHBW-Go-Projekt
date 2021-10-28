ee40a54b6893773ed7ffb9faa91b806c29df8414
Emil3005 <34603464+Emil3005@users.noreply.github.com>
2021-10-28 14:12:21 +0200

    Update configuration.go

--- src/model/config/configuration.go
+++ src/model/config/configuration.go
@@ -4,7 +4,7 @@ import "flag"
 
 var port1 string
 var port2 string
-var tokenDuration string
+var tokenDuration int
 var xmlPath string
 var logfilePath string
 var loginUrl string
@@ -23,16 +23,19 @@ var keyFile string
 //Other Paths
 
 func Init() {
-	SetFlags("8443", "8444", "60", "assets/locations.xml")
+	SetFlags("8443", "8444", 60, "assets/locations.xml")
 }
 
-func SetFlags(pPort1 string, pPort2 string, pTokenDur string, pXmlPath string) {
+func SetFlags(pPort1 string, pPort2 string, pTokenDur int, pXmlPath string) {
 	flag.StringVar(&port1, "port1", pPort1, "4-digit number for the port regarding the qrCodeScan")
 	flag.StringVar(&port2, "port2", pPort2, "4-digit number for the login Page")
-	flag.StringVar(&tokenDuration, "tokenDuration", pTokenDur, "The life duration of the token")
+	flag.IntVar(&tokenDuration, "tokenDuration", pTokenDur, "The life duration of the token")
 	flag.StringVar(&xmlPath, "xmlPath", pXmlPath, "Path to the xmlFile storing the locations")
 
 }
 func GetPath() string {
 	return xmlPath
+}
+func GetTokenDuration() int{
+	return tokenDuration
 }
